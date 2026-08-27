ee843ced6da68274e4a47f12e54f11292f58e45e
niraj-123-png <sg9223051@gmail.com>
2026-08-27 21:34:45 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const mineflayer = require('mineflayer');
+
+app.get('/', (req, res) => {
+    res.send('Mera Minecraft Bot chal raha hai!');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log('Web server start ho gaya.');
+});
+
+function createBot() {
+    const bot = mineflayer.createBot({
+        host: 'aapka-server-ip.com', // YAHAN APNI SMP KI IP DAALEIN
+        port: 25565,                 // Port change karein agar zaroorat ho
+        username: '24x7_AFK_Bot',    
+        version: '1.20.4'            // Apna server version daalein
+    });
+
+    bot.on('login', () => console.log('Bot server me join ho gaya!'));
+    
+    bot.on('end', () => {
+        console.log('Disconnect hua. Reconnecting...');
+        setTimeout(createBot, 10000);
+    });
+
+    bot.on('error', err => console.log('Error:', err));
+}
+
+createBot();
